5840606207d22cd3acc063b20b0ec3b2bbe1b669
Eli Albert <elialbert@gmail.com>
2020-07-04 16:43:14 -0500

    copy

--- squadmap/src/Components/About.svelte
+++ squadmap/src/Components/About.svelte
@@ -8,22 +8,22 @@
     <span class="float-right cursor-pointer" on:click={closeCB}>X</span>
   </h3>
   <div class='popover-body'>
-    <p class='p-3'>
+    <p class='p-2'>
       Squadmap.app is a tool to help you survive winter 2020. 
     </p>
-    <p class='p-3'>
-      The idea is to form a trusted squad of friends that you can hang out with indoors, and to be comfortable of the risk that entails.
+    <p class='p-2'>
+      The idea is to form a trusted squad of friends that you can hang out with indoors, and to be comfortable with the risk that entails.
     </p>
-    <p class='p-3'>
-      This tool helps you choose your squad by allowing you to visualize people and connections on a graph. Note that everything is automatically saved in YOUR browser - I don't want your data.
+    <p class='p-2'>
+      SquadMap.app helps you choose your squad by allowing you to visualize people and connections on a graph. Note that everything is automatically saved in YOUR browser - I don't want your data.
     </p>
-    <p class='p-3'>
-      Try it out! Click nodes and edges to change the risk and connection properties, drag nodes to fit on your screen better.
+    <p class='p-2'>
+      Try it out! Click nodes and edges to change the peoples' jobs and interactions, drag nodes to fit on your screen better.
     </p>
-    <p class='p-3'>
+    <p class='p-2'>
       If you're feeling adventurous, you can also change the numeric assumptions underlying the algorithm itself.
     </p>      
-    <p class='p-3'>
+    <p class='p-2'>
       Thanks, hope you like it! 
       <br/>
       <a href='https://elialbert.com'>Eli</a>
